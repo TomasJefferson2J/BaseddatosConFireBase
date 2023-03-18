c81f8fc6b8cf16dda28b5569d559631cbbe6b382
TomasJefferson2J <1303874@senati.pe>
2023-03-17 21:30:21 -0500

    basededatos

---
+++ app/src/main/java/com/example/mibasededatos/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mibasededatos
+
+import android.icu.text.CaseMap.Title
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //setup
+        setup()
+    }
+
+    private fun setup() {
+        title = "autentificacion"
+
+        SignUpButton
+
+
+
+    }
+}
